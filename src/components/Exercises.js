3c620538ec67f01b1293de9e3cdb5b7f0a45f809
Ian <ronesp.85@gmail.com>
2022-06-16 13:56:45 -0700

    [edit] able to search API for exercises and show gif images on exercise cards

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import Pagination from '@mui/material/Pagination'
+import { Box, Stack, Typography } from '@mui/material'
 
-const Exercises = () => {
+import { exerciseOptions, fetchData} from '../utils/fetchData'
+import ExerciseCard from './ExerciseCard'
+
+const Exercises = ({ exercises, setExercises, bodyPart}) => {
+  console.log(exercises)
   return (
-    <div>Exercises</div>
+    <Box
+      id="exercises"
+      sx={{
+        mt: {
+        lt: '110px'
+        }
+      }}
+      p="20px"
+      mt="50px"
+    >
+      <Typography variant='h3' mb="46px">
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: {lg: '110px', xs: '50px'}}}
+        flexWrap="wrap"
+        justifyContent="center"
+        >
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+    </Box>
   )
 }
 

df085fc9483929544c654dd430d6941672dd6bbb
Ian <ronesp.85@gmail.com>
2022-06-18 15:06:24 -0700

    [edit] ExerciseDetail card now is clickable and shows a gif of exercise

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Typography, Stack, Button } from '@mui/material'
 
-const Detail = () => {
+import BodyPartImage from '../assets/icons/body-part.png'
+import TargetImage from '../assets/icons/target.png'
+import EquipmentImage from '../assets/icons/equipment.png'
+
+const Detail = ({ exerciseDetail }) => {
+  const { boydPart, gifUrl, name, target, equipment } = exerciseDetail
+
+
   return (
-    <div>Detail</div>
+    <Stack
+      gap="60px"
+      sx={{ flexDirection: {
+        lg: 'row'
+        },
+      p: '20px',
+      alignItems: 'center'
+    }}>
+      <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+    </Stack>
   )
 }
 
